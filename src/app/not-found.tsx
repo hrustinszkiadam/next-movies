e18ccaa1b70252a8bf944ce8063ae8feae86ccef
hrustinszkiadam <hrustinszkiadam@gmail.com>
2025-11-05 13:17:54 +0100

    style (fix): not-found text centering

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,7 +2,7 @@ import BackButton from '@/components/back-button';
 
 export default function NotFound() {
   return (
-    <main className='flex min-h-screen w-screen flex-col items-center justify-center gap-4'>
+    <main className='flex min-h-screen w-screen flex-col items-center justify-center gap-4 text-center'>
       <h1 className='text-4xl font-bold'>404 - Az oldal nem található</h1>
       <p className='text-pretty'>
         Sajnáljuk, de az általad keresett oldal nem létezik.
